33dec202bfcfaa1622cb8a748082b3f07ccd42cb
Ayesha Siddiqa Khan <179512742+Ayesha-Siddiqa-khan@users.noreply.github.com>
2026-09-25 03:09:47 +0500

    fix(terraform): set force_delete = true on ECR repository for clean terraform destroy

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "main" {
   name                 = var.ecr_repository_name
   image_tag_mutability = var.ecr_image_tag_mutability
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = var.ecr_scan_on_push
